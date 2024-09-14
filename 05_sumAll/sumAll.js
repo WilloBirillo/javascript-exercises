8afebaf8d2abdd9c0ae3209299c3a93ee9d7da51
WilloBirillo <174868022+WilloBirillo@users.noreply.github.com>
2024-09-15 01:52:43 +0200

    Runs all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  const max = Math.max(num1, num2);
+  let sum = 0;
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1, num2) || typeof(num1, num2) != "number"
+  ) {
+    return "ERROR";
+  }
+  for (let i = max; i > 0; i--) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
