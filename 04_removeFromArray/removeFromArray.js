3bd1eeaf7a031a3644e87818e47259fc8c89f952
WilloBirillo <174868022+WilloBirillo@users.noreply.github.com>
2024-09-15 00:55:16 +0200

    2nd fix

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function (array, ...args) {
-  return array.filter((item) => !args.includes(item));
+  //return array.filter((item) => !args.includes(item));
+const new_array = [];
+  array.forEach((items) => {
+    if (!args.includes(items)) {
+        new_array.push(items);
+    }
+  });
+  return new_array;
 };
 
 // Do not edit below this line
