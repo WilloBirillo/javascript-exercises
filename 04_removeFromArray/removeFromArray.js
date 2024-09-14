945a9e25e9591eb8d9487f36503b9e73ac2afb95
WilloBirillo <174868022+WilloBirillo@users.noreply.github.com>
2024-09-15 00:41:40 +0200

    Fixed finally

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,6 @@
-const removeFromArray = function(array, ...args) {
-    for (const arg of args) {
-        array.splice(array.indexOf(arg), 1);
-    }
-    return array;
+const removeFromArray = function (array, ...args) {
+  return array.filter((item) => !args.includes(item));
 };
-/*
-const args = arguments;
-    const array = args[0];
-    array.splice(array.indexOf(args[1]), 1);
-    return array;
-    */
+
 // Do not edit below this line
 module.exports = removeFromArray;
