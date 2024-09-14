74cf5d2d27ebf7e7ebe3ae1d5701bd6dd8c383a9
WilloBirillo <174868022+WilloBirillo@users.noreply.github.com>
2024-09-14 21:06:40 +0200

    Fix: second test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,14 @@
-const removeFromArray = function(array, numb) {
-    const spliced = array.toSpliced((numb -1), 1);
-    return spliced;
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        array.splice(array.indexOf(arg), 1);
+    }
+    return array;
 };
-
+/*
+const args = arguments;
+    const array = args[0];
+    array.splice(array.indexOf(args[1]), 1);
+    return array;
+    */
 // Do not edit below this line
 module.exports = removeFromArray;
