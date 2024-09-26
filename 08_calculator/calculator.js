3e68d660c49edd1196b05f96b90de635c3fb98f3
WilloBirillo <174868022+WilloBirillo@users.noreply.github.com>
2024-09-26 21:18:02 +0200

    Passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, items) => sum += items, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, items) => product *= items, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let count = 1;
+	if (n === 0 || n === 1){
+    return 1;
+  } else {
+    for(let i = n; i > 0; i--) {
+      count *= i;
+    }
+    return count;
+  }
 };
 
 // Do not edit below this line
