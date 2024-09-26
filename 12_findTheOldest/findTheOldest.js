f3f7f1c68b92575b2a92724aaeacbf41b644a2c1
WilloBirillo <174868022+WilloBirillo@users.noreply.github.com>
2024-09-26 23:07:45 +0200

    Solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+/*const findTheOldest = function (people) {
+  return people.sort((a, b) => {
+    const lastGuyAge = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+    const nextGuyAge = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+    return nextGuyAge - lastGuyAge;
+  })[0];
+};*/
 
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
+
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
